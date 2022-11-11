92c99d9183e903fa1810ee68de75d41f61b97897
Jan Holcapek <holcapek@gmail.com>
2022-11-11 14:17:05 +0100

    feat: Add oci_always_free_infra_as_code repository

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ module "github_as_code" {
   initial_commit_author_name  = "Jan Holcapek"
   initial_commit_author_email = "holcapek@gmail.com"
 }
+
+module "oci_always_free_infra_as_code" {
+  source                      = "./modules/github-repository"
+  github_owner                = "holcapek"
+  github_token                = var.github_token
+  github_ssh_private_key_path = var.github_ssh_private_key_path
+  repository_name             = "oci-always-free-infra-as-code"
+  initial_commit_author_name  = "Jan Holcapek"
+  initial_commit_author_email = "holcapek@gmail.com"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "github_as_code_ssh_clone_url" {
   value = module.github_as_code.ssh_clone_url
 }
+
+output "oci_always_free_infra_as_code_ssh_clone_url" {
+  value = module.oci_always_free_infra_as_code.ssh_clone_url
+}
