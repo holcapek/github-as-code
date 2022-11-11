e12d6c3178f14207be127ab463c66bf9e766a09c
Jan Holcapek <holcapek@gmail.com>
2022-11-11 14:01:49 +0100

    feat: Add root module

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "github_as_code_ssh_clone_url" {
+  value = module.github_as_code.ssh_clone_url
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "github_token" {
+  type = string
+}
+
+variable "github_ssh_private_key_path" {
+  type = string
+}
